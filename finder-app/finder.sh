998a7e0119fe73c64a1bc44bcce1c5ff10651100
miljenkojakovljevic <miljenko.jakovljevic@gmail.com>
2024-04-13 10:34:56 +0200

    Added finder.sh

---
+++ finder-app/finder.sh
@@ -0,0 +1,20 @@
+if [ $# -lt 2 ]
+then
+	echo "Not all arguments supplied. Please provide filesdir and searchstr."   # h l  j k
+	exit 1
+fi
+
+filesdir=$1
+searchstr=$2
+
+if [ ! -d $filesdir ] 
+then
+	echo "Directory $filesdir does not exist."
+	exit 1
+fi
+
+filesnr=$(find $filesdir -type f | wc -l)
+matchinglines=$(find $filesdir -type f -exec grep -e $searchstr {} \; | wc -l)
+
+echo "The number of files are $filesnr and the number of matching lines are $matchinglines"
+
